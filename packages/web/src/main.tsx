ba36247e16ff57ca35fce62f8f87aa02e7609714
ElsaLundqvist <elsa.lundqvist.rr@gmail.com>
2026-06-28 14:31:49 +0200

    feat(web): setup Tailwind v4 con tokens @theme, fuentes y helper cn

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n/i18n.js";
 import { parseLocale } from "@kw/shared";
-import "./styles/global.scss";
+import "./index.css";
 import { App } from "./App.js";
 
 // Paridad con get_locale(): prioridad cookie kw_lang > fallback 'en'
